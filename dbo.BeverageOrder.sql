04a53cf8acdb874b7c512bc0269fb5553cb01ee9
dwentz28 <wentz.darin@gmail.com>
2016-09-06 22:11:30 -0400

    Add files via upload

---
+++ dbo.BeverageOrder.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[BeverageOrder] (
+    [BevOrder_Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [BevOrder_Status]   INT            DEFAULT ((0)) NOT NULL,
+    [Beverage_Id]       INT            NOT NULL,
+    [BevOrder_CustName] NVARCHAR (MAX) NOT NULL,
+    [BevOrder_Details] NVARCHAR(MAX) NULL, 
+    PRIMARY KEY CLUSTERED ([BevOrder_Id] ASC),
+    CONSTRAINT [FK_BeverageOrder_ToTable] FOREIGN KEY ([Beverage_Id]) REFERENCES [dbo].[Beverages] ([Beverage_Id]) 
+);
+
