b00b927822164190b2cb841e154c29756c079cd1
dwentz28 <wentz.darin@gmail.com>
2016-09-12 12:34:49 -0400

    Delete dbo.Beverages.sql

--- dbo.Beverages.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Beverages]
-(
-	[Beverage_Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Beverage_Name] NVARCHAR(MAX) NOT NULL, 
-    [Beverage_Description] NVARCHAR(MAX) NOT NULL, 
-    [Beverage_Price] DECIMAL(18, 2) NOT NULL
-)
